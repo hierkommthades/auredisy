0e92a5565d24e2067f2b9bc237ff16d4a5033b2b
Gravity <hier_kommt_hades@yahoo.de>
2020-11-13 12:11:42 -0400

    New commit applied

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
 # Configure the Azure provider
 terraform {
+
+
+
+  backend "azurerm" {
+    storage_account_name = "afgsstr000"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+    #use_msi              = true
+    subscription_id      = "fdfdb5ca-21e2-489a-927e-73f7122d9ddf"
+    tenant_id            = "2545989e-0855-40fc-8985-8d3bd416e901"
+    access_key           = "GvTvhrQdaHL8aKogNZEK8ofox/Z54xsiq+AeSbXCaKkep6wX6906daY9nwfwAEVuvnqCTCsSRTe1ammvxvzGdQ=="
+  }
+
+
+
+
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
